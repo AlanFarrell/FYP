3094038730a925485e87d323b36766a0dc199290
alan <alan@pop-os.localdomain>
2026-05-18 19:20:45 +0100

    test

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from CoverageMaps.CoverageTime import coverage_mapping
 from CoverageMaps.GroundTrack import plot_ground_tracks
 def main():
-    #this is a test
-    #coverage_mapping()
+    #this is another test
+    coverage_mapping()
     plot_ground_tracks()
 
 if __name__ == "__main__":
